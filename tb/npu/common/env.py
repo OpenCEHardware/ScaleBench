135fef53a8949e3651f48cebb976d1a96f82df50
Alejandro Diaz <alejandrodp@protonmail.com>
2025-06-07 12:20:14 -0600

    uvm_classes: adds test

---
+++ tb/npu/common/env.py
@@ -0,0 +1,41 @@
+import cocotb
+import pyuvm
+import logging
+from pyuvm import *
+from cocotb.clock import Clock
+from common.agents import *
+from common.scoreboard import *
+
+
+class NPUEnv(uvm_env):
+    def __init__(self, name="NPUEnv", parent=None):
+        super().__init__(name, parent)
+
+    def build_phase(self):
+        self.csr_agent = CSRAgent('csr_agent', self)
+        self.mem_agent = MemoryAgent('mem_agent', self)
+        self.scoreboard = NPUScoreboard('scoreboard', self)
+        self.mon_irq = IRQMonitor('mon_irq', self)
+
+        self.dut = ConfigDB().get(self, "", "dut")
+
+        ConfigDB().set(self, "*", "clk", self.dut.clk_npu)
+        ConfigDB().set(self, "*", "rst_n", self.dut.rst_n)
+        ConfigDB().set(self, "*", "vif", self.dut)
+        ConfigDB().set(self.mon_irq, "", "irq", self.dut.irq)
+
+    def connect_phase(self):
+        self.csr_agent.mon_csr_ar.ap.connect(self.scoreboard.csr_ar_fifo.analysis_export)
+        self.csr_agent.mon_csr_aw.ap.connect(self.scoreboard.csr_aw_fifo.analysis_export)
+        self.csr_agent.mon_csr_w.ap.connect(self.scoreboard.csr_w_fifo.analysis_export)
+        self.csr_agent.mon_csr_r.ap.connect(self.scoreboard.csr_r_fifo.analysis_export)
+        self.csr_agent.mon_csr_b.ap.connect(self.scoreboard.csr_b_fifo.analysis_export)
+
+        self.mon_irq.ap.connect(self.scoreboard.irq_fifo.analysis_export)
+
+    async def run_phase(self):
+        cocotb.start_soon(Clock(cocotb.top.clk_npu, 2).start())
+
+        self.dut.rst_n.value = 0
+        await ClockCycles(self.dut.clk_npu, 5)
+        self.dut.rst_n.value = 1
--- tb/npu/tests/tb_test.py
+++ tb/npu/tests/tb_test.py
@@ -3,8 +3,8 @@ import pyuvm
 import logging
 from pyuvm import *
 from cocotb.clock import Clock
-from common.agents import *
-from common.scoreboard import *
+from cocotb.triggers import RisingEdge
+from common.env import *
 
 
 @pyuvm.test()
@@ -12,37 +12,18 @@ class Test(uvm_test):
     """Simple test for ScaleNPU"""
 
     def build_phase(self):
-        self.csr_agent = CSRAgent('csr_agent', self)
-        self.mem_agent = MemoryAgent('mem_agent', self)
-        self.scoreboard = NPUScoreboard('scoreboard', self)
-        self.mon_irq = IRQMonitor('mon_irq', self)
-
+        self.env = NPUEnv('env', self)
         self.dut = cocotb.top
 
-        ConfigDB().set(None, "*", "clk", self.dut.clk_npu)
-        ConfigDB().set(None, "*", "rst_n", self.dut.rst_n)
-        ConfigDB().set(None, "*", "vif", self.dut)
-        ConfigDB().set(self.mon_irq, "", "irq", self.dut.irq)
-
-    def connect_phase(self):
-        self.csr_agent.mon_csr_ar.ap.connect(self.scoreboard.csr_ar_fifo.analysis_export)
-        self.csr_agent.mon_csr_aw.ap.connect(self.scoreboard.csr_aw_fifo.analysis_export)
-        self.csr_agent.mon_csr_w.ap.connect(self.scoreboard.csr_w_fifo.analysis_export)
-        self.csr_agent.mon_csr_r.ap.connect(self.scoreboard.csr_r_fifo.analysis_export)
-        self.csr_agent.mon_csr_b.ap.connect(self.scoreboard.csr_b_fifo.analysis_export)
-
-        self.mon_irq.ap.connect(self.scoreboard.irq_fifo.analysis_export)
+        ConfigDB().set(self.env, "", "dut", self.dut)
 
     async def run_phase(self):
         self.raise_objection()
 
-        cocotb.start_soon(Clock(cocotb.top.clk_npu, 2).start())
+        await RisingEdge(self.dut.rst_n)
 
-        self.dut.rst_n.value = 0
-        await ClockCycles(self.dut.clk_npu, 5)
-        self.dut.rst_n.value = 1
-
-        resp, data = await self.csr_agent.reg_block.ARCHID.read(self.csr_agent.reg_block.def_map, path_t.FRONTDOOR, check_t.NO_CHECK)
+        reg_block = self.env.csr_agent.reg_block
+        resp, data = await reg_block.ARCHID.read(reg_block.def_map, path_t.FRONTDOOR, check_t.NO_CHECK)
         assert (resp, data) == (status_t.IS_OK, 0xb00b)
 
         await ClockCycles(cocotb.top.clk_npu, 10)
